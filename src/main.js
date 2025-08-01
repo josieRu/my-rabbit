c574e181f134cb044b1a33ca253a502a0a7c99d8
userName <userEmail>
2025-08-01 18:58:43 +0800

    feat(apis): 添加 axios 并调用测试接口
    - 在 package.json 中添加 axios 依赖
    - 在 main.js 中导入并调用测试接口 getCategoryAPI
    - 更新 package-lock.json 相关依赖

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//测试接口函数
+import { getCategoryAPI } from '@/apis/test'
+
+getCategoryAPI().then(res => {
+    console.log(res)
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
